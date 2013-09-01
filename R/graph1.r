e943fc9424c2de2898f20593c4619215d6088521
Sebastien Rey <reyman64@gmail.com>
2013-09-01 20:44:53 +0200

    finishing creation of first R script for generating graphics and data used by graphics

--- R/graph1.r
+++ R/graph1.r
@@ -1,31 +1,10 @@
-#load lib
-library("ggplot2")
-library("plyr")
-
-#clean workspace
-rm(list=ls(all=TRUE)) 
-#large display
-options(width=300)
-
-# Utils function 
-# see http://stackoverflow.com/questions/4730551/making-a-string-concatenation-operator-in-r?lq=1
-String <- function(x) {
-  class(x) <- c("String", class(x))
-  x
-}
-
-"+.String" <- function(x,...) {
-  x <- paste(x, paste(..., sep="", collapse=""), sep="", collapse="")
-  String(x)
-}
-
-print.String <- function(x, ...) cat(x)
+source('common.r')
 
 # 1 = input file path for data (wait a csv)
 # 2 = output file path for png
 args <- commandArgs(TRUE)
 path = args[1]
-output <- String(args[2]) + "rk_variability.png"
+base_output = file_path_as_absolute(args[2]) 
 
 # Load data
 # need csv with "v_idn, v_ticks, v_seed, v_pop column" header to function
@@ -35,12 +14,19 @@ all_rk <- read.csv(path)
 rk_ordered <- ddply( all_rk, .(v_seed) , .fun= function(x) { x[order(-x$v_pop),] } )
 rk_ranked <- ddply( rk_ordered, .(v_seed) , transform, v_rank = sort(rank(v_pop, ties.method=c("first")))) 
 
-png(output)
 rk<- ggplot(rk_ranked, aes(x = v_rank,y=v_pop ))
 rk <- rk + geom_boxplot( aes(group = v_rank), size=0.2, fatten = 0.4 , outlier.colour = "red", outlier.size = 1.0) 
 rk <- rk + stat_summary(fun.y = mean,aes(shape="mean"), geom="point", color="red", shape=4) 
 rk <- rk + coord_trans(x = "log10", y = "log10") 
 rk <- rk + theme_bw() + xlab('Rank of cities') + ylab('Population of cities')
-rk
-garbage <- dev.off()
 
+print(basename(file_path_sans_ext(path)))
+
+svg_path <- String(basename(file_path_sans_ext(path))) + ".svg"
+csv_path <- String(basename(file_path_sans_ext(path))) + ".csv"
+  
+print("Saving " + svg_path + " in " + base_output + "\n") 
+ggsave(rk, file = svg_path, path=base_output ,dpi=600)
+
+print("Saving " + csv_path + " in " + base_output + "\n") 
+write.table(rk_ranked , csv_path, sep="," ,col.names=NA) 
